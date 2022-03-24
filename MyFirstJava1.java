2e6b2d432fb2ebcf6b987f6525bef4447700ee72
AlexeyChul <alexoro@yandex.ru>
2022-03-24 16:35:01 +0300

    First commit

---
+++ MyFirstJava1.java
@@ -0,0 +1,43 @@
+class MyFirstJava1 {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }   
+    
+    static void checkSumSign() {
+        int a = 5, b = 7;
+        if (a + b >=0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    } 
+    static void  printColor() {
+        int value = 10;
+        if (value <=0) {
+            System.out.println("Красный");
+        } 
+        if (value >0 && value <=100) {
+            System.out.println("Желтый");
+        }
+        if (value >100) {
+            System.out.println("Зеленый");
+        }
+    }
+    static void  compareNumbers() {
+        int a = 20, b = 30;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }    
+}    
